6d4769fba2367f06e5ae385b5b8ccf329cf97037
unknown <lamtran09>
2023-08-11 14:03:17 +0700

    first commit

---
+++ module_3/ss2_erd/exercise_2.sql
@@ -0,0 +1,32 @@
+create database ss2_erd_excercise_2;
+
+use ss2_erd_excercise_2;
+
+create table customer(
+	customer_id int primary key auto_increment,
+    customer_name varchar(45),
+    customer_age int
+);
+
+create table `order`(
+	order_id int primary key auto_increment,
+	customer_id int,
+    foreign key(customer_id) references customer(customer_id),
+    order_date date,
+    order_total_price int
+);
+
+create table product(
+	product_id int primary key auto_increment,
+    product_name varchar(45),
+    prouct_price int
+);
+
+create table order_detail(
+	order_id int,
+    product_id int,
+	primary key(order_id,product_id),
+	foreign key(order_id) references `order`(order_id),
+    foreign key(product_id) references product(product_id),
+    order_quantity int
+);
