52e37b0603dea2928bbecbde4a36ce728a76f2ea
unknown <lamtran09>
2023-08-30 07:39:28 +0700

    updates

---
+++ module_3/java_web/ss11_manager_product/db/db.sql
@@ -0,0 +1,16 @@
+create database ss11_product;
+
+use ss11_product;
+
+create table products(
+id int primary key auto_increment,
+`name` varchar(50),
+price int,
+`description` varchar(245),
+supplier varchar(45)
+);
+
+insert into products(`name`,price,`description`,supplier)
+values ("Americano",60000,"good","Highlands"),
+("Capuchino",50000,"very-much","Highlands"),
+("Cafe fin",29000,"very-much","Highlands");
